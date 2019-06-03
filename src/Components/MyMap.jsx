154b2a7f27c60268aabc2cd095abb44cfcf867f4
Tarrke <tarrke@gmail.com>
2019-06-03 20:42:26 +0200

    Generic layout

---
+++ src/Components/MyMap.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+
+export default class MyMap extends Component {
+    render() {
+        return (
+            <div className="Map">
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+        );
+    }
+}
---
+++ src/Components/MyMenu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MyMenu extends Component {
+    render() {
+        return (
+            <div className="Menu">
+                <h1>Menu</h1>
+            </div>
+        );
+    }
+};
