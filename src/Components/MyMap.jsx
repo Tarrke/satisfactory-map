b5600ef1948bb88cf2ace1934c170f770c1c6bfd
Tarrke <tarrke@gmail.com>
2019-06-05 08:58:32 +0200

    display leaflet map

--- src/Components/MyMap.jsx
+++ src/Components/MyMap.jsx
@@ -2,32 +2,49 @@ import React, { Component } from 'react';
 
 import { Map, Marker, Popup, TileLayer, ZoomControl } from 'react-leaflet'
 
+import * as DropPods from '../data/d_droppod'
+
 export default class MyMap extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            zoom: 5,
         };
+        const bounds= [[19,17],[-19,-17]]
+    }
+
+    componentDidMount() {
+        let mapInst = this.refs.map.leafletElement;
+        console.log( mapInst.getBounds() );
     }
 
     render() {
         var content = "Default";
         if (this.props.items.length > 0) {
             var items = this.props.items.map((item) => {
-                return <li>{item}</li>;
+                return <li key={item}>{item}</li>;
             });
             content = <ul>{items}</ul>;
         }
-        let position = [50, 50];
+        let position = [0, 0];
         return (
             <div className="Map">
-                <Map center={position}>
-                    <ZoomControl position="topright" />
-                    <Marker position={position}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
+                <Map ref='map'
+                    center={position}
+                    zoomSnap={0.5}
+                    maxZoom={7}
+                    minZoom={3}
+                    maxBounds={[[-406400, -406400], [508000, 508000]]}
+                    zoom={this.state.zoom}
+                >
+                    
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="/tiles/{z}/{x}/{y}.png"
+                        bounds={this.bounds}
+                        noWrap={true}
+                    />
+                    
                 </Map>
                 {content}
             </div>
---
+++ src/data/d_droppod.js
@@ -0,0 +1,23 @@
+export const dropPods = [
+    {
+        x: 87739.6796875,
+        y: -62975.45703125,
+        z: 13444.25,
+        originId: "PE_DP_1",
+        type: "droppod",
+        powerNeeded: 30,
+        obstructed: false,
+        collected: false
+      },
+      {
+        x: 232138,
+        y: 27191,
+        z: -1629,
+        originId: "PE_DP_10",
+        type: 'droppod',
+        itemQuantity: 5,
+        itemName: "Supercomputer",
+        obstructed: false,
+        collected:false
+      },
+]
